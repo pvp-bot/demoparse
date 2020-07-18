8468afc994995bfec266664feed5bb9fd5bff26a
Unknown <dhtxhg@gmail.com>
2020-07-18 02:00:22 -0700

    parse to csv

--- parsedemo.py
+++ parsedemo.py
@@ -2,50 +2,155 @@ import csv
 import shlex
 import sys
 import math
+import powers
 
 ms = 0          # time in ms
 t = 0           # time in seconds
-tick = 8        # tick rate to determine time stamps
+tick = 8        # tick rate to determine time stamps/time grouping
 
 player_names = []
 player_ids   = []
+player_list = []
+players = ''
 name_filter  = ['Mu Guardian','Phantasm','Decoy Phantasm','Decoy','Coralax Blue Hybrid','Dr','Poison Trap','Decoy','Animated Stone','Superior Vigilant Assault','Blind','Galvanic Sentinel','Voltaic Geyser','Voltaic Sentinel','Water Spout']
 
 pos_blue = [0,0] # red and blue are arbitrary, probably makes sense to be team 1 and 2
-pos_red  = [0,0]
 score    = [0,0]
+match_map = ""
+
+headers = ['time','actor','hp','deaths','team','action','target']
+
+class Player:
+	def __init__(self, name, pid):
+		self.name = name
+		self.id = pid
+		self.team = ''
+		self.deaths = 0
+		self.hp = ''
+		self.maxhp = 0.0
+		self.lasthp = 0.0
+		self.action = ''
+		self.target = ''
+	def reset(self):
+		self.hp = ''
+		self.action = ''
+		self.target = ''
 
 with open(sys.argv[1],'r') as fp:
-	line = shlex.split(fp.readline())
+	line = shlex.split(fp.readline().replace('\\',''))
 	count = 0
-	while line and count < 50000:
-		ms = ms + int(line[0]) # running demo time
-		t = round(ms*tick/1000)/tick # time in s rounded to the nearest server tick (manually set)
 
-		if len(line) > 0 and line[2] == "NEW":
-			if line[3] not in player_names and line[3] not in name_filter:
-				player_ids.append(line[1])
-				player_names.append(line[3])
+	# initializing line loop
+	while line and count < 20000:
+		try:
+			pid = int(line[1])
+		except:
+			pid = 0 # ignore special actors
 
-		# assign team id to player based on intial position - 
-		if line[2] == "POS" and line[1] in player_ids: # and team[player].id not assigned
-			if pos_blue == [0,0]:
-				pos_blue = [line[3],line[5]]
-			else:
-				dist = math.sqrt( ((float(pos_blue[0])-float(line[3]))**2)+((float(pos_blue[1])-float(line[5]))**2) )
-				if dist < 20:
-					pass # team[playerid] = blue
-				else:
-					pass # team[playerid] = red
 
-		# if line[2] == "HP":
-		#	player[id].hp  = float(line[3])
-		#	if player.hp == 0
-		#		team.deaths = team.deaths + 1
-		#		player.deaths = player.deaths + 1
+		if line[2] == "Map":
+			match_map = line[3]
+
+		if line[2] == "NEW":
+			if pid not in player_ids and line[3] not in name_filter:
+				player_ids.append(pid)
+				player_list.append(Player(line[3],pid))
 		
-		line = shlex.split(fp.readline()) #next line
+		try:
+			line = shlex.split(fp.readline()) #next line
+		except:
+			print(count)
 		count = count + 1
 
-players = dict(zip(player_ids,player_names))
-print(players)
+	players = dict(zip(player_ids,player_list))
+	
+	# back to start of file
+	fp.seek(0)
+	count = 0
+	line = shlex.split(fp.readline().replace('\\',''))
+	with open(sys.argv[2],'w',newline='') as csvfile:
+		csvw = csv.writer(csvfile, delimiter=',')
+		csvw.writerow(headers)
+
+	# main parsing loop
+	while line:
+		with open(sys.argv[2],'a',newline='') as csvfile:
+			csvw = csv.writer(csvfile, delimiter=',')
+			# print(count)
+			action = line[2]
+			try:
+				pid = int(line[1])
+			except:
+				pid = 0 # ignore special 
+
+			# assign team id to player based on intial position
+			# assumes demo record starts in the buff phase
+			if action == "POS" and pid in player_ids and players[pid].team == '':
+				if pos_blue == [0,0]:
+					pos_blue = [float(line[3]),float(line[5])]
+					players[pid].team = 1
+				else:
+					dist = math.sqrt( ((pos_blue[0]-float(line[3]))**2)+((pos_blue[1]-float(line[5]))**2) )
+					if dist < 20:
+						players[pid].team = 1
+					else:
+						players[pid].team = 2
+				# print("[" + str(players[pid].id) + "] " + "[TEAM " + str(players[pid].team) + "] "+ str(players[pid].name))
+
+
+			try: # 
+				if action == "HP" and pid in player_ids:
+					hp  = float(line[3])
+					if players[pid].lasthp != hp:
+						players[pid].hp = hp
+					if hp == 0:
+						players[pid].deaths = players[pid].deaths + 1
+
+				elif action == "FX" and line[3] == "OneShot" and pid in player_ids:
+					action = next(substring for substring in powers.plist if substring in line[5])
+					players[pid].action = powers.pdict[action]
+
+				elif action == "TARGET" and players[pid].action != '':
+					targ = int(line[4])
+					if targ in player_ids:
+						if targ != pid:
+							players[pid].target = players[int(targ)].name
+					else:
+						players[pid].target = targ
+
+
+			except:
+				# print(count)
+				pass
+
+			ms = ms + int(line[0]) # running demo time
+			t2 = round(ms*tick/1000)/tick # time in s rounded to the nearest server tick (manually set)
+			
+			if t2 > t:
+				for key, p in players.items():
+					csv_line = [t,p.name,p.hp,p.deaths,p.team,p.action,p.target]
+					csvw.writerow(csv_line)
+					p.reset()
+			t = t2
+			line = shlex.split(fp.readline().replace('\\','')) #next line
+			count = count + 1
+		
+
+
+
+print("")
+print("demo time " + str(t) + " seconds")
+print("map: " + match_map)
+print("")
+
+score1 = 0
+score2 = 0
+for key, p in players.items():
+	if p.team == 1:
+		score2 = score2 + p.deaths
+	else:
+		score1 = score1 + p.deaths
+	print("[TEAM " + str(p.team) + "]"+"[PID "+str(p.id)+"] " + p.name + " deaths: " + str(p.deaths))
+	
+print("")
+print("SCORE: " + str(score1) + "-" + str(score2))
