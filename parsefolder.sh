23a64e548083556ea37299879c44fdbe32b2c562
dt <dhtxhg@gmail.com>
2020-09-12 14:30:14 -0700

    script for running on folder

---
+++ parsefolder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# run parsedemo.py on all demos in folder
+# append resulting csvs together w/ name of folder
+
+dir_name=$(basename $1)
+
+for demo in $1/*.cohdemo
+do
+	python parsedemo.py "$demo"
+done
+
+first="0"
+for csv in $1/*.csv
+do
+	if [ "$first" -eq "0" ]
+	then
+		head -1 $csv > "$1"/"$dir_name".csv
+		first="1"
+	fi
+	tail -n +2 -q $csv >> "$1"/"$dir_name".csv
+done
